428b69ce8bb56e4bead297f08f867ee4c29c2ff3
Mevorah Berrebi <berrebimevo@gmail.com>
2026-01-04 11:42:55 +0200

    first test

---
+++ tests/movies.test.js
@@ -0,0 +1,21 @@
+beforeAll(done => {
+    // Any setup needed before tests run
+    console.log('Starting Movies API tests...');
+
+    done();
+});
+
+afterAll(done => {
+    // Any cleanup needed after tests run
+    console.log('Finished Movies API tests.');
+    done();
+});
+
+describe('Movies API', () => {
+    test("test 1 ", () => {
+        // Your test code here
+        console.log("movies test 1");
+        const stam = 5;
+        expect(stam).toBe(5);
+    });
+});
