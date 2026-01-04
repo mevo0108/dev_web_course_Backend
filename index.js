9fdf807b825608cf2c86f032666b07d46d1cba4e
Mevorah Berrebi <berrebimevo@gmail.com>
2026-01-05 00:01:41 +0200

    fix pb with dev run and restAPI working

--- index.js
+++ index.js
@@ -17,4 +17,9 @@ app.use(express.json());
 const moviesRouter = require('./routes/moviesRoute');
 app.use('/movie', moviesRouter);
 
+// 404 handler for routes not found
+app.use((req, res) => {
+    res.status(404).json({ status: 'error', message: 'Route not found' });
+});
+
 module.exports = app;
--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const moviesController = require('../Controllers/moviesControllers');
 
+// GET all movies and POST new movie
 router.get('/', moviesController.getAllMovies);
-
-router.get('/:id', moviesController.getMovieById);
-
 router.post('/', moviesController.createMovie);
 
-router.delete('/:id', moviesController.deleteMovie);
-
+// GET, PUT, DELETE by ID (must come after root routes)
+router.get('/:id', moviesController.getMovieById);
 router.put('/:id', moviesController.updateMovie);
+router.delete('/:id', moviesController.deleteMovie);
 
 module.exports = router;
