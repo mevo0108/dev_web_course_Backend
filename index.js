14630edd48c51b4dce71497a40255d5d503137a0
Mevorah Berrebi <berrebimevo@gmail.com>
2026-01-04 23:49:55 +0200

    adding more tests and coverage, need to fix the dev and start API.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -10,6 +11,7 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const moviesRouter = require('./routes/moviesRoute');
--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,24 +1,108 @@
 const request = require('supertest');
 const app = require('../index'); // Adjust the path as necessary
+const movies = require('../models/moviesModel');
 
-beforeAll(done => {
+var testData = [
+    {
+        title: "Inception",
+        year: 2010
+    },
+    {
+        title: "The Matrix",
+        year: 1999
+    },
+    {
+        title: "Interstellar",
+        year: 2014
+    }
+
+]
+
+beforeAll(async () => {
     // Any setup needed before tests run
-    console.log('Starting Movies API tests...');
 
-    done();
+    await movies.deleteMany({});
 });
 
-afterAll(done => {
+afterAll((done) => {
     // Any cleanup needed after tests run
     console.log('Finished Movies API tests.');
     done();
 });
 
 describe('Movies API', () => {
-    test("test 1 ", async () => {
+    test("test get all empty DB ", async () => {
         // Your test code here
-        console.log("movies test 1");
-        const stam = 5;
-        expect(stam).toBe(5);
+        console.log("test is running");
+        const response = await request(app).get('/movie');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual([]);
+
     });
+
+    test("test add a movie", async () => {
+        //add all test data
+
+        for (const movie of testData) {
+            const response = await request(app)
+                .post('/movie')
+                .send(movie);
+            expect(response.statusCode).toBe(201);
+            expect(response.body).toMatchObject(movie);
+        }
+
+
+    });
+
+    test("test get all movies after adding", async () => {
+
+        const response = await request(app).get('/movie');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toBe(testData.length);
+    });
+
+    test("test get movie by filter", async () => {
+        const movie = testData[0];
+        const response = await request(app).get(
+            '/movie?year=' + movie.year
+        );
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toBe(1);
+        expect(response.body[0].year).toBe(movie.year);
+        testData[0]._id = response.body[0]._id; // Save the ID for later tests
+
+
+    });
+
+    test("test get movie by id", async () => {
+        const response = await request(app).get('/movie/' + testData[0]._id);
+        expect(response.statusCode).toBe(200);
+        expect(response.body._id).toBe(testData[0]._id);
+    });
+
+
+    test("test put movie by id", async () => {
+        testData[0].year = 2010;
+        const updatedData = {
+            title: "Inception Updated",
+            year: 2012
+        };
+        const response = await request(app)
+            .put('/movie/' + testData[0]._id)
+            .send(testData[0]);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.title).toBe(testData[0].title);
+        expect(response.body.year).toBe(testData[0].year);
+    });
+
+
+
+    test("test delete a movie", async () => {
+        const response = await request(app).delete('/movie/' + testData[0]._id);
+        expect(response.statusCode).toBe(200);
+
+        const getResponse = await request(app).get('/movie/' + testData[0]._id);
+        expect(getResponse.statusCode).toBe(404);
+    });
+
 });
